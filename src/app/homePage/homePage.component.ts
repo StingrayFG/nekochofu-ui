4c4838ff71c1a90530cb35e8ab874e2f1a705a61
Igor <54187585+StingrayFG@users.noreply.github.com>
2024-04-17 19:21:48 +0300

    Add home page

---
+++ src/app/homePage/homePage.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import axios from 'axios';
+
+@Component({
+  selector: 'app-page',
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, CdkTextareaAutosize],
+  templateUrl: './homePage.component.html',
+  styleUrl: './homePage.component.scss'
+})
+
+export class HomePageComponent {
+  lastOriginalValue = '';
+  newLink = '';
+  
+  showingMessage = false;
+  message = ''
+
+  contentsControl = new FormControl()
+
+  copyLink(): void {
+    if (this.newLink) {
+      this.showMessage('Copied!')
+      navigator.clipboard.writeText(this.newLink);
+    }
+  }
+
+  async showMessage(msg: string) {
+    this.message = msg;
+    this.showingMessage = true;
+    await new Promise(res => setTimeout(res, 1500));
+    this.showingMessage = false;
+  }
+
+  async createRecord() {
+    console.log(this.contentsControl.value)
+    if (this.contentsControl.status === 'VALID') {
+      if (this.contentsControl.value !== this.lastOriginalValue) {
+        this.newLink = '...'
+        await axios.post('http://localhost:8000/record/create', {contents: this.contentsControl.value})
+        .then(res => {
+          this.newLink = 'localhost:4200' + '/' + res.data;
+          this.lastOriginalValue = this.contentsControl.value!;
+        })
+        .catch(err => {
+          this.newLink = '';
+          this.showMessage('Something went wrong');
+        })
+      }
+    } else {
+      this.showMessage('Enter a correct link');
+    }
+  }
+}
